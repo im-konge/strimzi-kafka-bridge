abf9aa710dc4bad11bc8104c1479a331899e33d8
morsak <xorsak02@stud.fit.vutbr.cz>
2020-07-24 09:47:11 +0200

    [MO] - testing with pipeline
    Signed-off-by: morsak <xorsak02@stud.fit.vutbr.cz>

--- src/test/java/io/strimzi/kafka/bridge/http/ConsumerTest.java
+++ src/test/java/io/strimzi/kafka/bridge/http/ConsumerTest.java
@@ -1410,7 +1410,6 @@ public class ConsumerTest extends HttpBridgeTestBase {
         assertThat(context.awaitCompletion(TEST_TIMEOUT, TimeUnit.SECONDS), is(true));
     }
 
-    @DisabledIfEnvironmentVariable(named = "EXTERNAL_BRIDGE", matches = "((?i)TRUE(?-i))")
     @Test
     void tryReceiveNotValidJsonMessage(VertxTestContext context) throws InterruptedException, ExecutionException, TimeoutException {
         String topic = "tryReceiveNotValidJsonMessage";
